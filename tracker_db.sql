3a2d858a065ff410f734ac5a3148e7b5a5843fa2
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-02-18 09:27:59 -0800

    update

--- tracker_db.sql
+++ tracker_db.sql
@@ -3,58 +3,36 @@ CREATE DATABASE tracker_db;
 
 USE tracker_db;
 
-CREATE TABLE department
-(
-  id INT
-  UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR
-  (50) UNIQUE NOT NULL
+CREATE TABLE department(
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL
 );
 
-  SELECT *
-  FROM department;
+SELECT * FROM department;
 
-  CREATE TABLE role
-  (
-    id INT
-    UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR
-    (50) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
-  department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind
+CREATE TABLE role(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(50) UNIQUE NOT NULL,
+	salary DECIMAL UNSIGNED NOT NULL,
+	department_id INT UNSIGNED NOT NULL,
+	INDEX dep_ind
     (department_id),
-  CONSTRAINT fk_department FOREIGN KEY
-    (department_id) REFERENCES department
-    (id) ON
-    DELETE CASCADE
+	CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-    SELECT *
-    FROM role;
+    SELECT * FROM role;
 
-    CREATE TABLE employee
-    (
-      id INTEGER NOT NULL
-      AUTO_INCREMENT,
-    first_name VARCHAR
-      (50) NOT NULL,
-    last_name VARCHAR
-      (50) NOT NULL,
+CREATE TABLE employee(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER,
-    PRIMARY KEY
-      (id),
-    INDEX dep_ind
-      (department_id),
-	CONSTRAINT fk_department FOREIGN KEY
-      (role_id) REFERENCES role
-      (id) ON
-      DELETE CASCADE
+    PRIMARY KEY(id),
+    INDEX dep_ind(department_id),
+	CONSTRAINT fk_department FOREIGN KEY(role_id) REFERENCES role(id) ON DELETE CASCADE
 );
 
-      SELECT *
-      FROM employee;
+SELECT * FROM employee;
 
-      ALTER USER 'root'@'localhost' IDENTIFIED
-      WITH mysql_native_password BY 'chargers619';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'chargers619';
